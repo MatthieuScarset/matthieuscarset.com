5aa87a70535f4ba29e5307cfd746b18f448a9a7c
Matthieu SCARSET <m@matthieuscarset.com>
2023-05-08 13:05:22 -0500

    Update

---
+++ web/themes/custom/matthieu/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.twig"],
+  theme: {
+    fontFamily: {
+      'icons': ['Material', 'sans-serif']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
